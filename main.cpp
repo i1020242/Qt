be3cb237542335d7dc4a1310c0c6055b26f3409e
i1020242 <mrnix@crunchbang.(none)>
2015-08-08 13:12:49 +0700

    change main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
-#include<QApplication>
 #include"systemtray.h"
 
 
 int main(int argv, char** argc)
 {
     QApplication a(argv, argc);
-    cSystemTray *window = new cSystemTray;
-    window->resize(300,400);
-    window->setWindowIcon(QIcon(":/icon"));
-    window->setWindowTitle("Message SystemTray");
-    window->show();
+    cSystemTray window;
+    window.setWindowTitle("SystemTray Application");
+    window.setWindowIcon(QIcon(":/icon.jpeg"));
+    window.show();
+
     return a.exec();
 }
